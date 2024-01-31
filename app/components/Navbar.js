b3e718e6f59f360fa5ce9532da0f4f7d1c0b2ef7
Mishan Poudel <poudelmishan2@gmail.com>
2024-01-31 22:36:50 +0000

    feat: create and add Navbar component

---
+++ app/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        Mishan Poudel
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Item 1</a></li>
+      <li>
+          <summary>Parent</summary>
+      </li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn">Button</a>
+  </div>
+</div>
+  )
+}
