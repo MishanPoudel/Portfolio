8e8478fe9643c87b4a99b2365eabbc9c62bc4a02
Mishan Poudel <poudelmishan2@gmail.com>
2024-02-01 11:49:04 +0000

    feat: implement custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
+  daisyui: {
+    themes: [
+      {
+        misery: {
+          primary: "#f3f4f6",
+          secondary: "#111827",
+          accent: "#0000ff",
+          neutral: "#323232",
+          "base-100": "#1b1b1b",
+          info: "#0089ad",
+          success: "#a6d200",
+          warning: "#f79700",
+          error: "#df2f3b",
+        },
+      },
+      {
+        serenity: {
+          primary: "#292524",
+          secondary: "#f3f4f6",
+          accent: "#0000ff",
+          neutral: "#323232",
+          "base-100": "#e0e0e0",
+          info: "#0089ad",
+          success: "#a6d200",
+          warning: "#f79700",
+          error: "#df2f3b",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
