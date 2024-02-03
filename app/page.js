b94ee159c87a719ed3ae59e6705a4eee5a4a0198
Mishan Poudel <poudelmishan2@gmail.com>
2024-02-03 20:45:03 +0000

    style: add responsiveness for large (lg) devices

--- app/page.js
+++ app/page.js
@@ -1,67 +1,67 @@
 export default function Home() {
   return (
     <>
-      <main className="mx-60 my-5 px-2">
-        <div className="flex">
-          <div className="h-[35vh] flex-grow shadow-inset rounded-3xl">
-            {/* About  */}
+      <main className="mx-4 md:mx-8 lg:mx-60 lg:mt-6 mt-9">
+        <div className="flex flex-col lg:flex-row">
+          <div className="lg:h-[35vh] h-[61.5vh] flex-grow shadow-inset rounded-3xl mb-4 lg:mb-0">
+            About
           </div>
-          <div className="flex flex-col w-1/2">
-            <div className="shadow-inset flex h-12 rounded-3xl">
-              {/* feautre slider */}
+          <div className="flex flex-col lg:w-1/2">
+            <div className="shadow-inset flex rounded-3xl justify-end mb-4 lg:mb-2 lg:h-10 items-center px-2">
+            Latest Work and Feature
+            </div>
+            <div className="flex flex-col grow lg:flex-row lg:mt-2">
+              <div className="shadow-inset rounded-3xl mb-4 lg:mb-0 lg:w-1/2">
+                About me
               </div>
-            <div className="flex flex-grow">
-              <div className="shadow-inset grow rounded-3xl">
-                {/* about me */}
-                </div>
-              <div className="shadow-inset w-1/2 rounded-3xl">
-                {/* clock */}
-                </div>
+              <div className="shadow-inset lg:w-1/2 rounded-3xl">
+                Clock
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex">
-          <div className="flex-grow w-1/2 h-52 rounded-3xl shadow-inset">
-            {/* specilization */}
-            </div>
-          <div className="grow h-52 rounded-3xl shadow-inset w-1/2">
-            <div className="flex justify-even items-center h-full px-2">
+        <div className="flex flex-col lg:flex-row lg:mt-5">
+          <div className="flex-grow lg:w-1/2 h-52 rounded-3xl shadow-inset mb-4 lg:mb-0">
+            Specialization
+          </div>
+          <div className="grow h-52 rounded-3xl shadow-inset lg:w-[35vw] md:w-full">
+            <div className="flex justify-around items-center h-full px-2">
               <div className="shadow-inset rounded-3xl h-36 w-40">
-                {/* years */}
-                </div>
+                Years
+              </div>
               <div className="shadow-inset rounded-3xl h-36 w-40">
-                {/* clients */}
-                </div>
+                Clients
+              </div>
               <div className="shadow-inset rounded-3xl h-36 w-40">
-                {/* projects */}
-                </div>
+                Projects
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex h-64">
-          <div className="shadow-inset grow rounded-3xl">
-            {/* blog */}
-            </div>
-          <div className="shadow-inset grow rounded-3xl w-1/4 px-3 py-5">
+        <div className="flex flex-col lg:flex-row h-64">
+          <div className="shadow-inset grow rounded-3xl mb-4 lg:mb-0">
+            Blog
+          </div>
+          <div className="shadow-inset grow rounded-3xl lg:w-1/4 px-3 py-5 mb-4 lg:mb-0">
             <div className="shadow-inset grow rounded-3xl h-1/2">
-              {/* connections */}
-              </div>
-            {/* profile */}
+              Connections
             </div>
-          <div className="shadow-inset rounded-3xl w-1/4">
-            {/* work together */}
-            </div>
-        </div>
-          <div className="mt-20 text-3xl font-bold text-center">Mishan Poudel</div>
-        <div className="flex justify-center">
-          <div className="flex justify-evenly w-1/3 my-12">
-            <div>Home</div>
-            <div>About</div>
-            <div>Work</div>
-            <div>Contact</div>
+            Profile
+          </div>
+          <div className="shadow-inset rounded-3xl lg:w-[16vw]">
+            Work together
           </div>
         </div>
-        <div className="text-center mt-5">Created by <span className="text-blue-400">Mishan Poudel</span></div>
+        <div className="lg:mt-24 lg:mb text-3xl font-bold text-center">Mishan Poudel</div>
+        <div className="flex justify-center">
+          <div className="flex flex-wrap justify-evenly w-full my-8 md:w-auto">
+            <div className="mb-2 md:mb-0 md:mr-4">Home</div>
+            <div className="mb-2 md:mb-0 md:mr-4">About</div>
+            <div className="mb-2 md:mb-0 md:mr-4">Work</div>
+            <div className="mb-2 md:mb-0">Contact</div>
+          </div>
+        </div>
+        <div className="text-center m-5 lg:mb-32">Created by <span className="text-blue-400">Mishan Poudel</span></div>
       </main>
     </>
   );
