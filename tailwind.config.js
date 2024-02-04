471c8f753057700a94d7371d57191c814f9315d9
Mishan Poudel <poudelmishan2@gmail.com>
2024-02-04 19:26:33 +0000

    style: add marquee text and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,5 +34,23 @@ module.exports = {
       },
     ],
   },
+  theme: {
+    extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+    },
+  },
   plugins: [require("daisyui")],
 };
