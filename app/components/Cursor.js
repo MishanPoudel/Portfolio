339389815f50ba83d8c7b7d8f6fade863e1a64cf
Mishan Poudel <poudelmishan2@gmail.com>
2024-02-04 20:59:24 +0000

    chore: add dependency for mouse movement

--- app/components/Cursor.js
+++ app/components/Cursor.js
@@ -3,36 +3,48 @@ import React, { useEffect } from 'react';
 import { motion, useMotionValue, useSpring } from 'framer-motion';
 
 export default function Cursor() {
-    const cursorSize = 20;
+  const cursorSize = 20;
   const mouse = {
     x: useMotionValue(0),
     y: useMotionValue(0)
   };
 
-  const smoothOptions = {damping:20, stiffness:300, mass:0.5}
+  const smoothOptions = { damping: 20, stiffness: 300, mass: 0.5 };
 
   const smoothMouse = {
     x: useSpring(mouse.x, smoothOptions),
     y: useSpring(mouse.y, smoothOptions)
-  }
-  
-  const manageMouseMove = (e) => {
-    const { clientX, clientY } = e;
-    mouse.x.set(clientX - cursorSize /2)
-    mouse.y.set(clientY - cursorSize /2)
   };
 
   useEffect(() => {
+    const manageMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      mouse.x.set(clientX - cursorSize / 2);
+      mouse.y.set(clientY - cursorSize / 2);
+    };
+
     window.addEventListener("mousemove", manageMouseMove);
+
     return () => {
       window.removeEventListener("mousemove", manageMouseMove);
     };
-  }, []); 
+  }, [mouse.x, mouse.y, cursorSize]);
 
-  console.log(smoothMouse.x, smoothMouse.y)
   return (
     <>
-      <motion.div style={{ left: smoothMouse.x, top: smoothMouse.y, background: "#f3f4f6", height: cursorSize, width: cursorSize, position: 'fixed', borderRadius: "50%", pointerEvents: "none" }}>
+      <motion.div
+        style={{
+          zIndex: 1000,
+          left: smoothMouse.x,
+          top: smoothMouse.y,
+          background: "#f3f4f6",
+          height: cursorSize,
+          width: cursorSize,
+          position: 'fixed',
+          borderRadius: "50%",
+          pointerEvents: "none"
+        }}
+      >
       </motion.div>
     </>
   );
