ef5f8c8226c61d73a6835a33e63401b091e3dd4e
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-03-25 21:18:44 +0000

    ui: Implement placeholder images for self portrait section

---
+++ app/components/RenderImage.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+// const self1 = require("/public/self1.jpeg");
+// const self2 = require("/public/self2.jpeg");
+// const self3 = require("/public/self3.jpeg");
+
+let self1 =
+  "https://i.pinimg.com/736x/17/a9/af/17a9af82ac1dda57893544af6e1c2c6a.jpg";
+let self2 =
+  "https://i.pinimg.com/736x/92/4b/f5/924bf5338641f57f48183da4eccffdf3.jpg";
+let self3 =
+  "https://i.pinimg.com/736x/38/15/f6/3815f6bf5f1c669d85df1135d8db720a.jpg";
+
+const RenderImage = () => {
+  return (
+    <>
+      <div className="w-full flex justify-center font-extralight select-none">
+        *supposed to be self pic but anime pic for now*
+      </div>
+      <div className="grid grid-cols-3 gap-4 py-4 select-none">
+        <img
+          src={self2}
+          width="3072"
+          height="4096"
+          alt="Self Photo 2"
+          className="rounded-2xl"
+        />
+        <img
+          src={self1}
+          width="3072"
+          height="4096"
+          alt="Self Photo 1"
+          className="rounded-2xl"
+        />
+        <img
+          src={self3}
+          width="3072"
+          height="4096"
+          alt="Self Photo 3"
+          className="rounded-2xl"
+        />
+      </div>
+    </>
+  );
+};
+
+export default RenderImage;
