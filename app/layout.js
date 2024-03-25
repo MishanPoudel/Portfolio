594c250bf6e48306f34e14009de3ce97c209eca3
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-03-25 21:25:20 +0000

    feat: Implement application layout and basic SEO

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,32 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Hanken_Grotesk } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const hankenGrotesk = Hanken_Grotesk({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio",
-  description: "Explore my diverse collection of projects showcasing my skills and experiences in web development, design, and more.",
+  title: "Mishan Poudel",
+  description: "Uncover deeper insights into my journey and capabilities.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${hankenGrotesk.className} scroll-smooth antialiased bg-bg_black text-text_white`}
+      >
+        <div className="mx-6 md:max-w-xl md:mx-auto">
+          <Navbar />
+          {children}
+          <Footer />
+          <SpeedInsights />
+          <Analytics />
+        </div>
+      </body>
     </html>
   );
 }
