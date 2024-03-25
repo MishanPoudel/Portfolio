50c6f980a2891829318333d5a0c408c8567ad7d8
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-03-25 21:25:53 +0000

    feat: Implement Home page showcasing personal introduction and skills

--- app/page.js
+++ app/page.js
@@ -1,7 +1,26 @@
+import { skills, profileDescription } from "./data/data";
+import SkillItem from "./components/SkillItem";
+import RenderImage from "./components/RenderImage";
+import Heading from "./components/Heading";
+
 export default function Home() {
   return (
     <main>
-      <h1>hello world</h1>
+      <div>
+        <Heading heading="Mishan" />
+        <section>
+          <p className="text-self">{profileDescription[0]}</p>
+          <RenderImage />
+          <p className="text-self">{profileDescription[1]}</p>
+          <div className="text-self">{profileDescription[2]}</div>
+          <div className="text-self">{profileDescription[3]}</div>
+          <div className="flex flex-wrap justify-center gap-3">
+            {skills.map((skill) => (
+              <SkillItem skillItem={skill} key={skill.key} />
+            ))}
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
